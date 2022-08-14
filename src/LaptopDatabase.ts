11530b940320f13e71d1d08940aee3ffa14f77a6
Nish Tahir <nishtahir@outlook.com>
2022-08-13 23:15:38 -0400

    Setup lint and code style checks

--- src/LaptopDatabase.ts
+++ src/LaptopDatabase.ts
@@ -1,24 +1,24 @@
 const LaptopDatabase: LaptopInformation[] = [
-    {
-        manufacturer: 'Acer',
-        modelNumber: 'Helios 300 (2021)',
-        cpus: [
-            'Intel i7-10750H',
-            'Intel i7-11800H'
-        ],
-        gpus: [
-            {
-                name: 'NVIDIA RTX 2070',
-                switchableGraphicsTechnology: SwitchableGraphicsTechnology.NvidiaAdvancedOptimus,
-                isDedicated: true
-            },
-            {
-                name: 'NVIDIA RTX 3070',
-                switchableGraphicsTechnology: SwitchableGraphicsTechnology.NvidiaAdvancedOptimus,
-                isDedicated: true
-            }
-        ],
-        muxStatus: MuxStatus.No,
-        notes: ''
-    }
-];
+  {
+    manufacturer: 'Acer',
+    modelNumber: 'Helios 300 (2021)',
+    cpus: [
+      'Intel i7-10750H',
+      'Intel i7-11800H'
+    ],
+    gpus: [
+      {
+        name: 'NVIDIA RTX 2070',
+        switchableGraphicsTechnology: SwitchableGraphicsTechnology.NvidiaAdvancedOptimus,
+        isDedicated: true
+      },
+      {
+        name: 'NVIDIA RTX 3070',
+        switchableGraphicsTechnology: SwitchableGraphicsTechnology.NvidiaAdvancedOptimus,
+        isDedicated: true
+      }
+    ],
+    muxStatus: MuxStatus.No,
+    notes: ''
+  }
+]
